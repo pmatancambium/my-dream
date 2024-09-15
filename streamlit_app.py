95e7341ff037e02e38461178c4bd59cadd722859
MatanyaP <matanperetz314@gmail.com>
2024-09-15 15:09:09 +0300

    add style reference

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,8 @@ from email.header import Header
 from deep_translator import GoogleTranslator
 import json
 from user_data_storage import user_storage
+import asyncio
+import traceback
 
 # if user_data is not in st.session_state, add it with default values
 if "user_data" not in st.session_state:
@@ -90,10 +92,12 @@ def translate_text(text):
     return translator.translate(text)
 
 
-def send_email(subject, body, image_data, username):
+def send_email(subject, body, image_data, username, additional_recipient=None):
     msg = MIMEMultipart()
     msg["From"] = EMAIL_ADDRESS
     msg["To"] = RECIPIENT_EMAIL
+    if additional_recipient:
+        msg["Cc"] = additional_recipient
     
     # Get the user's image filename without extension
     user_to_file = st.secrets["user_to_file"]
@@ -114,7 +118,10 @@ def send_email(subject, body, image_data, username):
         with smtplib.SMTP("smtp.gmail.com", 587) as server:
             server.starttls()
             server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-            server.send_message(msg)
+            recipients = [RECIPIENT_EMAIL]
+            if additional_recipient:
+                recipients.append(additional_recipient)
+            server.sendmail(EMAIL_ADDRESS, recipients, msg.as_string())
         return True
     except Exception as e:
         st.error(f"An error occurred while sending the email: {str(e)}")
@@ -150,51 +157,83 @@ def upload_image_to_leonardo(image_file):
 
 
 def generate_image_leonardo(prompt, init_image_id, preset_style):
-    if not init_image_id:
-        raise Exception("init_image_id is required")
-    
     headers = {
         "Authorization": f"Bearer {LEONARDO_API_KEY}",
         "Content-Type": "application/json",
     }
 
     escaped_prompt = prompt.replace("'", "\\'")
-    payload = {
+
+    # First, generate an image based on the prompt alone
+    initial_payload = {
         "prompt": escaped_prompt,
         "modelId": "1e60896f-3c26-4296-8ecc-53e2afecc132",  # Leonardo Diffusion XL
         "presetStyle": preset_style,
         "photoReal": True,
-        "photoRealVersion":"v2",
-        "alchemy":True,
-        "num_images": 4,  # Changed to generate 4 images
+        "photoRealVersion": "v2",
+        "alchemy": True,
+        "num_images": 1,
+        "enhancePrompt": True,
+    }
+
+    initial_response = requests.post(LEONARDO_API_URL, json=initial_payload, headers=headers)
+
+    if initial_response.status_code != 200:
+        raise Exception(f"Failed to generate initial image: {initial_response.text}")
+
+    initial_generation_id = initial_response.json()["sdGenerationJob"]["generationId"]
+
+    # Poll for the generated initial image
+    while True:
+        status_response = requests.get(f"{LEONARDO_API_URL}/{initial_generation_id}", headers=headers)
+        if status_response.status_code == 200:
+            generation_data = status_response.json()["generations_by_pk"]
+            if generation_data["status"] == "COMPLETE":
+                generated_image_id = generation_data["generated_images"][0]["id"]
+                break
+        time.sleep(5)  # Wait for 5 seconds before polling again
+
+    # Now, generate the final images using both the uploaded and generated images as references
+    final_payload = {
+        "prompt": escaped_prompt,
+        "modelId": "1e60896f-3c26-4296-8ecc-53e2afecc132",  # Leonardo Diffusion XL
+        "presetStyle": preset_style,
+        "photoReal": True,
+        "photoRealVersion": "v2",
+        "alchemy": True,
+        "num_images": 4,
         "enhancePrompt": True,
         "controlnets": [
             {
                 "initImageId": init_image_id,
                 "initImageType": "UPLOADED",
-                "preprocessorId": 133, # Character Reference Id
+                "preprocessorId": 133,  # Character Reference Id
                 "strengthType": "High",
+            },
+            {
+                "initImageId": generated_image_id,
+                "initImageType": "GENERATED",
+                "preprocessorId": 67,  # Character Reference Id
+                "strengthType": "Mid",
             }
         ] if init_image_id else [],
     }
 
-    response = requests.post(LEONARDO_API_URL, json=payload, headers=headers)
+    final_response = requests.post(LEONARDO_API_URL, json=final_payload, headers=headers)
 
-    if response.status_code == 200:
-        generation_id = response.json()["sdGenerationJob"]["generationId"]
+    if final_response.status_code == 200:
+        generation_id = final_response.json()["sdGenerationJob"]["generationId"]
 
         # Poll for the generated images
         while True:
-            status_response = requests.get(
-                f"{LEONARDO_API_URL}/{generation_id}", headers=headers
-            )
+            status_response = requests.get(f"{LEONARDO_API_URL}/{generation_id}", headers=headers)
             if status_response.status_code == 200:
                 generation_data = status_response.json()["generations_by_pk"]
                 if generation_data["status"] == "COMPLETE":
                     return [image["url"] for image in generation_data["generated_images"]]
             time.sleep(5)  # Wait for 5 seconds before polling again
     else:
-        raise Exception(f"Failed to generate image: {response.text}")
+        raise Exception(f"Failed to generate final images: {final_response.text}")
 
 
 # Initialize session state
@@ -202,18 +241,114 @@ if "authenticated" not in st.session_state:
     st.session_state.authenticated = False
 
 
+def success_page():
+    st.title("Success!")
+    st.success("התמונה והפרומפט נשלחו בהצלחה")
+    st.write("ניתן לסגור חלון זה עכשיו או להמשיך לאפליקציה הראשית.")
+
 # Streamlit app
 def main():
-    if st.session_state.get("email_sent_success", False):
-        success_page()
-    elif not st.session_state.get("authenticated", False):
+    if "page" not in st.session_state:
+        st.session_state.page = "main"
+
+    if not st.session_state.get("authenticated", False):
         login_page()
-    elif st.session_state.get("show_loading", False):
-        loading_page()
-    elif st.session_state.get("show_generated_images", False):
+        return
+
+    # Create a container for the main content
+    main_container = st.empty()
+
+    if st.session_state.page == "main":
+        with main_container.container():
+            if "error_message" in st.session_state:
+                st.error(st.session_state.error_message)
+                del st.session_state.error_message
+
+            st.title("חלום בתמונה")
+            st.write("אנא תארו חלום או דמיון שלכם. למשל, 'אני חולם לשחות עם להקת כרישים מסוכנים באוקיינוס' (עד 200 תוים).")
+            st.write("מדובר בערב חברתי. נשמח לחלומות ודמיונות קלילים ומשעשעים.")
+
+            dream_description = st.text_area("בחלומי אני...", max_chars=200, height=100, placeholder="לדוגמה: במקום מסוים, עם אדם או חיה וכו'...")
+
+            if st.button("צור תמונה", type="primary"):
+                if user_storage.can_generate_image(st.session_state.username):
+                    if dream_description:
+                        st.session_state.complete_text = dream_description
+                        st.session_state.page = "loading"
+                        st.experimental_rerun()
+                    else:
+                        st.warning("אנא תארו את החלום שלכם לפני יצירת התמונה.")
+                else:
+                    st.error("הגעת למספר המקסימלי של תמונות שאתה יכול ליצור. אנא נסה שוב מאוחר יותר.")
+
+            if st.button("Logout"):
+                st.session_state.authenticated = False
+                st.experimental_rerun()
+
+    elif st.session_state.page == "loading":
+        with main_container.container():
+            st.title("יוצר את התמונה שלך...")
+            st.warning("אנא אל תסגור את הדף או תרענן אותו בזמן שאנחנו יוצרים את התמונה שלך.")
+            
+            with st.spinner("מעבד את החלום שלך..."):
+                status_text = st.empty()
+                
+                # Display random fun facts while processing
+                while True:
+                    status_text.text(random.choice(fun_facts))
+                    time.sleep(5)  # Update fun fact every 5 seconds
+                    
+                    # Check if images are ready
+                    processed_images = asyncio.run(generate_images_async())
+                    if processed_images:
+                        break
+
+            if processed_images:
+                st.session_state.processed_images = processed_images
+                st.session_state.page = "show_images"
+                st.experimental_rerun()
+            else:
+                st.session_state.page = "main"
+                st.experimental_rerun()
+
+    elif st.session_state.page == "show_images":
         show_generated_images_page()
-    else:
-        main_page()
+    
+    elif st.session_state.page == "success":
+        success_page()
+
+async def generate_images_async():
+    try:
+        user_image = load_user_image(st.session_state.username)
+        init_image_id = None
+        thumbnail_image = None
+        if user_image:
+            img_byte_arr = BytesIO()
+            user_image.save(img_byte_arr, format='JPEG')
+            img_byte_arr.seek(0)
+            
+            init_image_id = upload_image_to_leonardo(img_byte_arr)
+            thumbnail_image = user_image
+
+        complete_text_english = translate_text(st.session_state.complete_text)
+        complete_text_english = "This is a picture of me. Place me according to the description: I am" + complete_text_english
+
+        image_urls = generate_image_leonardo(complete_text_english, init_image_id, "UNPROCESSED")
+
+        processed_images = []
+        for url in image_urls:
+            response = requests.get(url)
+            img = Image.open(BytesIO(response.content))
+
+            if thumbnail_image:
+                img = overlay_thumbnail(img, thumbnail_image)
+
+            processed_images.append(img)
+
+        return processed_images
+    except Exception as e:
+        st.session_state.error_message = f"An error occurred: {str(e)}\n\n{traceback.format_exc()}"
+        return None
 
 def login_page():
     st.title("Login")
@@ -258,102 +393,6 @@ def load_user_image(username):
             return Image.open(image_path)
     return None
 
-def success_page():
-    st.title("Success!")
-    st.success("התמונה והפרומפט נשלחו בהצלחה")
-    st.write("You can close this window now or return to the main app.")
-    
-    # Add a button to go back to the main app
-    # if st.button("Return to Main Page"):
-    #     st.session_state.processed_images = None
-    #     st.session_state.selected_image = None
-    #     st.session_state.complete_text = None
-    #     st.experimental_rerun()
-
-def loading_page():
-    st.title("יוצר את התמונה שלך...")
-    st.warning("אנא אל תסגור את הדף או תרענן אותו בזמן שאנחנו יוצרים את התמונה שלך.")
-
-    # Replace progress bar with a spinner
-    with st.spinner("מעבד את החלום שלך..."):
-        status_text = st.empty()
-
-        for i in range(20):
-            if i % 4 == 0:
-                random_fact = random.choice(fun_facts)
-                status_text.markdown(f"<div class='ltr'>{random_fact}</div>", unsafe_allow_html=True)
-            time.sleep(0.5)
-
-    try:
-        user_image = load_user_image(st.session_state.username)
-        init_image_id = None
-        thumbnail_image = None
-        if user_image:
-            img_byte_arr = BytesIO()
-            user_image.save(img_byte_arr, format='JPEG')
-            img_byte_arr.seek(0)
-            
-            init_image_id = upload_image_to_leonardo(img_byte_arr)
-            thumbnail_image = user_image
-
-        complete_text_english = translate_text(st.session_state.complete_text)
-        complete_text_english = "This is a picture of me. Place me according to the description: I am" + complete_text_english
-
-        image_urls = generate_image_leonardo(complete_text_english, init_image_id, "UNPROCESSED")
-
-        processed_images = []
-        for url in image_urls:
-            response = requests.get(url)
-            img = Image.open(BytesIO(response.content))
-
-            if thumbnail_image:
-                img = overlay_thumbnail(img, thumbnail_image)
-
-            processed_images.append(img)
-
-        st.session_state.processed_images = processed_images
-        st.session_state.show_loading = False
-        st.session_state.show_generated_images = True
-        st.rerun()
-
-    except Exception as e:
-        st.error(f"אירעה שגיאה בעת יצירת התמונות. אנא נסה שוב מאוחר יותר.")
-        st.session_state.show_loading = False
-        st.rerun()
-
-def app():
-    if st.session_state.get("email_sent_success", False):
-        success_page()
-        return
-
-    if st.session_state.get("show_generated_images", False):
-        show_generated_images_page()
-    else:
-        main_page()
-
-def main_page():
-    st.title("חלום בתמונה")
-    st.write("אנא תארו חלום או דמיון שלכם. למשל, 'אני חולם לשחות עם להקת כרישים מסוכנים באוקיינוס' (עד 200 תוים).")
-    st.write("מדובר בערב חברתי. נשמח לחלומות ודמיונות קלילים ומשעשעים.")
-
-    dream_description = st.text_area("בחלומי אני...", max_chars=200, height=100, placeholder="לדוגמה: במקום מסוים, עם אדם או חיה וכו'...")
-
-    if st.button("צור תמונה", type="primary"):
-        if user_storage.can_generate_image(st.session_state.username):
-            if dream_description:
-                complete_text = f"{dream_description}"
-                st.session_state.complete_text = complete_text
-                st.session_state.show_loading = True
-                st.rerun()
-            else:
-                st.warning("אנא תארו את החלום שלכם לפני יצירת התמונה.")
-        else:
-            st.error("הגעת למספר המקסימלי של תמונות שאתה יכול ליצור. אנא נסה שוב מאוחר יותר.")
-
-    if st.button("Logout"):
-        st.session_state.authenticated = False
-        st.rerun()
-
 def show_generated_images_page():
     st.title("התמונות שנוצרו")
     st.write("בחר את התמונה שברצונך לשלוח:")
@@ -374,13 +413,13 @@ def show_generated_images_page():
 
                 email_subject = "New Dream Image Generated"
                 email_body = f"A new dream image has been generated with the following prompt:\n\n{st.session_state.complete_text}"
+                additional_recipient = st.secrets["ADDITIONAL_RECIPIENT"]
                 
                 with st.spinner("שולח את התמונה"):
-                    if send_email(email_subject, email_body, img_byte_arr, st.session_state.username):
+                    if send_email(email_subject, email_body, img_byte_arr, st.session_state.username, additional_recipient=additional_recipient):
                         user_storage.set_last_email_sent(st.session_state.username)
-                        st.session_state.email_sent_success = True
-                        st.session_state.show_generated_images = False  # Add this line
-                        st.rerun()
+                        st.session_state.page = "success"
+                        st.experimental_rerun()
                     else:
                         st.error("לא הצלחנו לשלוח את התמונה והפרומפט")
 
@@ -392,22 +431,9 @@ def show_generated_images_page():
         st.session_state.processed_images = None
         st.session_state.selected_image = None
         st.session_state.complete_text = None
-        st.session_state.show_generated_images = False
+        st.session_state.page = "main"
         user_storage.increment_image_count(st.session_state.username)
-        st.rerun()
-
-# Add this function outside of the app() function
-def send_email_callback(complete_text, img_byte_arr):
-    email_subject = "New Dream Image Generated"
-    email_body = f"A new dream image has been generated with the following prompt:\n\n{complete_text}"
-    
-    with st.spinner("שולח את התמונה"):
-        if send_email(email_subject, email_body, img_byte_arr, st.session_state.username):
-            st.success("התמונה והפרומפט נשלחו בהצלחה")
-            user_storage.set_last_email_sent(st.session_state.username)
-        else:
-            st.error("לא הצלחנו לשלוח את התמונה והפרומפט")
-
+        st.experimental_rerun()
 
 if __name__ == "__main__":
     main()
